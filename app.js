7fc62dcb3a2169a3a448d875747ac63a98d5714d
Joaquinlp11 <joaquinlp58@gmail.com>
2023-05-08 20:02:26 -0300

    version 0.2

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+const rutaPublic = path.resolve(__dirname, './public');
+
+app.use(express.static(rutaPublic));
+
+app.listen(3000, () => console.log('funcionando'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
