c6ffeaadea40616defeca230f5f661411962cb85
Joaquinlp11 <joaquinlp58@gmail.com>
2023-09-11 15:16:52 -0300

    cambios 1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const rutaPublic = path.resolve(__dirname, './public');
 
 app.use(express.static(rutaPublic));
 
-app.listen(3000, () => console.log('funcionando'));
+app.listen(3000, () => console.log('funcionando '));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
