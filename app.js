c4d3fa382055665bb9e5b55e1d922372057b4008
Joaquinlp11 <joaquinlp58@gmail.com>
2023-09-12 18:31:25 -0300

    desploy settings

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
-
 const app = express();
-
 const path = require('path');
 
 const rutaPublic = path.resolve(__dirname, './public');
 
 app.use(express.static(rutaPublic));
 
-app.listen(3000, () => console.log('funcionando'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('funcionando en el puerto: ' + port));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
